7b6af5282af6451fe4d30c84ba200a304e16a91e
Z Yin <Z.Yin.MD@gmail.com>
2018-05-10 23:48:37 -0400

    add react icons

--- client/src/Components/Choices/Choices.js
+++ client/src/Components/Choices/Choices.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Choices.css'
+import Cross from 'react-icons/lib/md/clear';
 const Choices = () => (
   <ol>
-    <li>👎👎这根本不能算名字 / 任何人都不会喜欢</li>
+    <li>
+    <i><Cross /></i>
+      这根本不能算名字 / 任何人都不会喜欢
+    </li>
+    <li>
+      <i></i>
+      ★还行, 提供了一点灵感
+    </li>
     <li>👎我不喜欢, 别人或许有一定概率喜欢</li>
-    <li>★还行, 提供了一点灵感</li>
     <li>❤好名字, 备选</li>
   </ol>
 );
