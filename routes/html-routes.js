a5c40f91121f8bbb69ea73ded7f85213ab20641e
Z Yin <Z.Yin.MD@gmail.com>
2018-05-12 01:13:17 -0400

    change / to /* in html route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,4 @@
 module.exports = function (app) {
-    app.get('/', (req, res) => res.sendFile('../client/build/index.html'));
+    app.get('/*', (req, res) => res.sendFile('../client/build/index.html'));
 };
 
