a213f4e04b67c69149b13fa6e92cf269d99cdaba
Z Yin <Z.Yin.MD@gmail.com>
2018-05-12 01:22:24 -0400

    try deploy again

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true })); // if req url contains query string, this middleware parses that and stores in req.query
 app.use(bodyParser.json()); // if req.body is in json, this middleware parses that into object
-app.use(express.static('client/build/'));
+app.use(express.static('client/build'));
 require('./routes/api-routes.js')(app);
 require('./routes/html-routes.js')(app);
 
