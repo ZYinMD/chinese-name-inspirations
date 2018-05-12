56ffbeedd96989b28f9a42ba4e54c89437308939
Z Yin <Z.Yin.MD@gmail.com>
2018-05-12 02:56:16 -0400

    don't know what to say

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,7 @@
 const path = require('path');
 module.exports = function (app) {
-    console.log('__dirname: ', (path.join(__dirname, '../client/build/index.html')));
     app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, './client/build/index.html'));
+      res.sendFile(path.join(__dirname, 'client/build/index.html'));
     });
 };
 
