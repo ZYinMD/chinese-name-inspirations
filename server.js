00ca463d8f82eaab6d165a9c2987c0f7148a3277
Z Yin <Z.Yin.MD@gmail.com>
2018-05-12 03:26:52 -0400

    finally seems to be fixed

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.use(bodyParser.json()); // if req.body is in json, this middleware parses th
 app.use(express.static('client/build'));
 
 require('./routes/api-routes.js')(app);
-// require('./routes/html-routes.js')(app);
+require('./routes/html-routes.js')(app);
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 
 app.listen(PORT, () => console.log('App listening on PORT ' + PORT));
