fd3b76e68ffa9b8a580bd7fe3bac9c18412666ad
Z Yin <Z.Yin.MD@gmail.com>
2018-06-16 22:10:06 -0400

    delete yarn.lock manually

--- client/src/Components/Choices/Choices.js
+++ client/src/Components/Choices/Choices.js
@@ -24,19 +24,19 @@ const Choices = ({nameObj, submit, collapseRef, updateLastRating}) => {
 
   return (
     <ol className='choices'>
-      <li className='choice' onClick={() => {handleClick(1)}}>
+      <li className='choice' onClick={() => {handleClick(1);}}>
         <i className='cross theme'><Cross /></i>
         <p>正常人不可能<br/>起这个名字</p>
       </li>
-      <li className='choice' onClick={() => {handleClick(2)}}>
+      <li className='choice' onClick={() => {handleClick(2);}}>
         <i className='face-icon theme'><Meh /></i>
         <p>我不喜欢, 别人或许<br/>有一定概率喜欢</p>
       </li>
-      <li className='choice' onClick={() => {handleClick(3)}}>
+      <li className='choice' onClick={() => {handleClick(3);}}>
         <i className='theme'><Bulb /></i>
         <p>还行, 提供了<br/>一点灵感</p>
       </li>
-      <li className='choice' onClick={() => {handleClick(4)}}>
+      <li className='choice' onClick={() => {handleClick(4);}}>
         <i className='heart theme'><Heart /></i>
         <p>好名字, 备选</p>
       </li>
