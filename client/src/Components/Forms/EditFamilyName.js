fe8e63d76aeda742b1f67d080a9f08a23632b2cf
Z Yin <Z.Yin.MD@gmail.com>
2018-06-16 22:04:02 -0400

    delete yarn.lock

--- client/src/Components/Forms/EditFamilyName.js
+++ client/src/Components/Forms/EditFamilyName.js
@@ -8,6 +8,10 @@ class EditFamilyName extends Component {
     当前姓: window.settings.姓,
   }
 
+  handleFocus = event => { // select the text on load
+    event.target.select();
+  }
+
   handleInputChange = event => {
     const { value } = event.target;
     this.setState({当前姓: value.slice(-1)});
@@ -19,7 +23,7 @@ class EditFamilyName extends Component {
       <div className='edit-family-name'>
         <Header leftIcon={<Back/>} leftLink={'/settings'} title={'修改宝宝的姓'} headingLevel={3}/>
         <h4>宝宝的姓: </h4>
-        <form><input type="text" autoFocus  value = {this.state.当前姓} onChange={this.handleInputChange} /></form>
+        <form><input type="text" autoFocus  value = {this.state.当前姓} onFocus={this.handleFocus} onChange={this.handleInputChange} /></form>
       </div>
     );
   }
