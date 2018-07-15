6c8d3969695945af2516df5024eca84d892f032d
Z Yin <Z.Yin.MD@gmail.com>
2018-07-15 10:31:53 -0400

    re-order chars

--- db/one-time-scripts/create-just-chars-with-labels.js
+++ db/one-time-scripts/create-just-chars-with-labels.js
@@ -2,6 +2,7 @@ const input = require('../db-exports/characters.json');
 const output = '../one-time-data/just-chars-with-labels.json';
 const fs = require('fs');
 const dataStorage = {};
+input.sort((a, b) => Number(a._id.slice(-4)) - Number(b._id.slice(-4)));
 for (let i of input) {
   dataStorage[i.char] = i.labels;
 }
